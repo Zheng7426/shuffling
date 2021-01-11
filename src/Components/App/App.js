c5d26fa4e5646b9db842d49ebf5799fb48e8c44d
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-01-11 00:43:03 -0500

    components' directories generated

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Shu<span className="highlight">ffl</span>ing</h1>
+        <div className="App">
+          
+          <div className="App-playlist">
+            
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
