9139d2bcecd3f89b9185d73df6de73ddb6a912bf
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-01-12 13:01:37 -0500

    Pass Down Search Result and Render Result List

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,44 @@
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'homesick',
+          artist: 'kings of convenience',
+          album: 'riot in an empty street',
+          id: 1
+        },
+        {
+          name: 'When we were young',
+          artist: 'adele',
+          album: '25',
+          id: 2
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
         <h1>Shu<span className="highlight">ffl</span>ing</h1>
         <div className="App">
           <SearchBar />
-          <SearchResults />
+          <SearchResults searchResults={this.state.searchResults} />
           <div className="App-playlist">
             <Playlist />
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Track.css';
 
+
 class Track extends React.Component {
+
     render() {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3></h3>
-                    <p>|</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist}|{this.props.track.album}</p>
                 </div>
                 <button className="Track-action"></button>
             </div>
