2dfececdff10f88f5b73c3c7828c15be186f0789
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-01-12 00:27:23 -0500

    SearchResults component initiated

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+
 
 function App() {
   return (
     <div>
         <h1>Shu<span className="highlight">ffl</span>ing</h1>
         <div className="App">
-          
+          <SearchBar />
+          <SearchResults />
           <div className="App-playlist">
 
           </div>
